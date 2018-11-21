84647c9a58fc3d0ea13ebba43315f589b5952b2f
robengrobban <kastanjen3@gmail.com>
2018-11-21 22:09:55 +0100

    Create connect-database.php

---
+++ php/connect-database.php
@@ -0,0 +1,14 @@
+<?php
+//Skapa anslutnings variabler
+$serverip = "213.112.218.202";
+
+$username = "robneogy";
+
+$password = "BQw3obdO8w9YhQ7v";
+
+$database = "databaseGy";
+
+//Skapa anslutningsobjekt
+$conn = new mysqli( $serverip , $username , $password , $database );
+
+?>
