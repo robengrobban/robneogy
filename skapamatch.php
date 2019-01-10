662a0003aa8c0fe992667f7f8711be910061815b
robengrobban <kastanjen3@gmail.com>
2019-01-10 11:28:06 +0100

    Struktur

---
+++ skapamatch.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="sv">
+	<head>
+		<!--STANDARD INITIERING-->
+		<title>Robot Wars Nacka Gymnasium</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<!--STIL MED CSS-->
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+
+	</head>
+	<body>
+		<p>Apa</p><br>
+		<p>banan</p>
+		
+	</body>
+</html>
