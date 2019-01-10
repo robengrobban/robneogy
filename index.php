7a0e1752470d0a81a2c82f50d8c5d1b4d910d831
robengrobban <kastanjen3@gmail.com>
2019-01-10 12:19:42 +0100

    Start på start sidan

--- index.php
+++ index.php
@@ -8,11 +8,42 @@
 
 		<!--STIL MED CSS-->
 		<link rel="stylesheet" type="text/css" href="css/main.css">
-
+		<link rel="stylesheet" type="text/css" href="css/nav.css">
+		<link rel="stylesheet" type="text/css" href="css/index.css">
 	</head>
 	<body>
-		<p>Apa</p><br>
-		<p>banan</p>
-		
+
+		<!--HEADER FÖR FRONT TEXT OCH BAKGRUNDSBILD-->
+		<header id="front-page-header">
+			<h1>Robot Wars Nacka Gymnasium</h1>
+		</header>
+
+		<!--NAV FÖR STARTSIDAN-->
+		<nav id="main-nav">
+			<ul>
+				<li><a href="#">Test</a></li>
+				<li><a href="#">Test</a></li>
+				<li><a href="#">Test</a></li>
+				<li><a href="#">Test</a></li>
+			</ul>
+		</nav>
+
+		<!--SECTION FÖR UPPKOMMANDE MATCHER-->
+		<div id="upcoming-matches">
+			<h2>Uppkommande matcher</h2>
+			<div>
+				<p>
+					<span class= "team-one">LAG 1</span>
+					<span class="team-vs">VS</span>
+					<span class="team-two">LAG 2</span>
+				</p>
+			</div>
+		</div>
+
+		<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+		<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+		<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+		<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
 	</body>
 </html>
