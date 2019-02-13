eb7b75b6320363c57f486111bd77ff3c83be0669
Ne0nw0w <neo@legzdins.se>
2019-02-13 11:00:21 +0100

    Update resetPassword.php

--- php/resetPassword.php
+++ php/resetPassword.php
@@ -1,6 +1,8 @@
 <?php  
 include 'include/main-include.php';
-include 'php/include/clear-data.php';
+include 'include/clear-data.php';
+include 'include/session-start.php';
+
 
 //kolla att deras verikey stämmer.
 //använd POST för att hämta ut deras lösenord och sätt det som ['']
@@ -8,13 +10,40 @@ include 'php/include/clear-data.php';
 //update user set password det dom skriver
 //låt dem sedan skriva in nytt lösenord, med POST sätt det nya.
 
+//Kolla ifall användaren är inloggad
+include 'include/is-logged-in.php';
+if ( isLoggedIn() ) {
+	header("Location: ../index.php");
+}
 
 if (isset($_GET['userEmail'])&& isset($_GET['userName'])&& isset($_GET['veriKey'])) {
+	$email = clearData($_GET['userEmail']);
+	$name = clearData($_GET['userName']);
+	$key = clearData($_GET['veriKey']);
+
+	include 'include/connect-database.php';
+	$stmt = $conn->prepare("SELECT count(*) FROM account WHERE mail = ? AND username = ? AND veriKey = ? "); 
+	$stmt->bind_param("sss",$email, $name, $key);
+	$stmt->execute();
+
+	$res = $stmt->get_result()->fetch_all(MYSQLI_ASSOC)[0]['count(*)'];
+
+
+	if ($res != 1) {
+			header("Location: error.php?error-msg=Åtkomst nekad!");	
+	}
 	
+
+	$stmt->close();
+	$conn->close();
+
 }
 else{
 	header("Location: error.php?error-msg=Åtkomst nekad!");
+
 }
+
+
 
 
 
@@ -33,7 +62,7 @@ else{
 		<!--STIL MED CSS-->
 		<link rel="stylesheet" type="text/css" href="../css/main.css">
 		<link rel="stylesheet" type="text/css" href="../css/nav.css">
-		<link rel="stylesheet" type="text/css" href="../css/rsPw.css">
+		<link rel="stylesheet" type="text/css" href="../css/skapaKonto.css">
 
 		
 </head>
@@ -44,7 +73,13 @@ else{
 				<li><a href="index.php">Hem</a></li>
 			</ul>
 		</nav>
+<?php  
+#pungsäck
+if (isset($_POST['reset'])&& isset($_POST['password']) &&isset($_POST['password-rep'])) {
+	
+}
 
+?>
 		<!--FROM FÖR INLOGG-->
 		<form method="POST">
 
@@ -61,7 +96,7 @@ else{
 				<input required type="password" name="password-rep" placeholder="Upprepa lösenord">
 			</div>
 			
-			<button type="submit" name="create">Återställ Lösenord</button>
+			<button type="submit" name="reset">Återställ Lösenord</button>
 
 		</form>
 
