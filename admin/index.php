5433d3d71eac01b76c39f588eb665cfde03a3fe2
robengrobban <kastanjen3@gmail.com>
2019-05-08 10:25:08 +0200

    Start Admin

---
+++ admin/index.php
@@ -0,0 +1,16 @@
+<?php
+include '../php/include/main-include.php';
+include '../php/include/session-start.php';
+?>
+<!DOCTYPE html>
+<html lang="sv">
+	<head>
+		<!--STANDARD INITIERING-->
+		<title>Robot Wars Nacka Gymnasium</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	</head>
+	<body>
+
+	</body>
+</html>
